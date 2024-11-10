a7ddbe8f34c82a4bc2d85ded94a0c0bda5e70a6d
Ashutosh <ashuma2721@gmail.com>
2024-11-10 20:04:36 +0530

    remived unused imports

--- packages/client/src/app/components/QuizGameCardsSection.tsx
+++ packages/client/src/app/components/QuizGameCardsSection.tsx
@@ -3,7 +3,6 @@ import {
   Card,
   CardBody,
   CardFooter,
-  Divider,
   Heading,
   Stack,
   Text,
--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Spinner, Stack, Text, useDisclosure } from "@chakra-ui/react";
+import { Spinner, Stack, Text, useDisclosure } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import {
   CreateQuizRoomEventData,
